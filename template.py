3e1fdc8da391d7ef14c30fd1653017f2fd5ccbd6
dinesh-gaire <132206290+dinesh-gaire@users.noreply.github.com>
2024-01-19 07:02:50 +0000

    Folder structure updated

--- template.py
+++ template.py
@@ -1,46 +1,44 @@
 import os
 from pathlib import Path
-import logging
 
-logging.basicConfig(level=logging.INFO)
-
-project_name = "mlproject"
-
-list_of_files = [
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/components/data_ingestion.py",
-    f"src/{project_name}/components/data_transformation.py",
-    f"src/{project_name}/components/model_trainer.py",
-    f"src/{project_name}/components/model_monitering.py",
-    f"src/{project_name}/pipelines/training_pipeline.py",
-    f"src/{project_name}/pipelines/prediction_pipeline.py",
-    f"src/{project_name}/exception.py",
-    f"src/{project_name}/logger.py",
-    f"src/{project_name}/utils.py",
-    "app.py",
-    "Dockerfile",
+list_of_files=[
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/components/__init__.py",
+    "src/components/data_ingestion.py",
+    "src/components/data_transformation.py",
+    "src/components/model_trainer.py",
+    "src/components/model_evaluation.py",
+    "src/pipeline/__init__.py",
+    "src/pipeline/training_pipeline.py",
+    "src/pipeline/prediction_pipeline.py",
+    "src/utils/__init__.py",
+    "src/utils/utils.py",
+    "src/logger/logging.py",
+    "src/exception/exception.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
     "requirements.txt",
-    "setup.py"
+    "requirements_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "pyproject.toml",
+    "tox.ini",
+    "experiment/experiments.ipynb"
 
 ]
-
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
-    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,'w') as f:
-            pass
-            logging.info(f"Creating empty file: {filepath}")
+        with open(filepath, "w") as f:
+            pass # create an empty file
 
 
-    
-    else:
-        logging.info(f"{filename} is already exists")
+
+
